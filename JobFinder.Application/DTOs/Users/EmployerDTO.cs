8a34a8c4dc8b64d2c0f75287948732cb82fdd309
Mateja989 <79813462+Mateja989@users.noreply.github.com>
2023-08-31 13:45:10 +0200

    Update EmployerDTO.cs

--- JobFinder.Application/DTOs/Users/EmployerDTO.cs
+++ JobFinder.Application/DTOs/Users/EmployerDTO.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JobFinder.Domain.Entities;
 
 namespace JobFinder.Application.DTOs.Users
 {
